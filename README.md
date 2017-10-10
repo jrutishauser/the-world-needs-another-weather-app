6cfb4de4ff4b745790bd73d0f7ae86e3c44b4bdb
Jon Rutishauser <jrutishauser@gmail.com>
2017-10-10 01:44:05 -0400

    When a city name is entered weather info is shown
    When you search a city, weather stats are displayed for that city. The
    first td also shows a google map section of the city. When another city
    is searched it is added to the top of the list.

---
+++ README.md
@@ -0,0 +1,4 @@
+# The world needs another crappy weather app
+
+Just a react/redux weather app that uses google maps, openweathermap api and
+sparklines.
---
+++ src/components/chart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Sparklines, SparklinesLine, SparklinesReferenceLine} from 'react-sparklines';
+import _ from 'lodash';
+
+export default (props) => {
+  return (
+    <div>
+      <Sparklines height={120} width={180} data={ props.data }>
+        <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
+      </Sparklines>
+      <div>{_.round(_.sum(props.data)/props.data.length)} {props.units}</div>
+    </div>
+  )
+}
